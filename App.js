c82c4d5cc3802de00f9ab18132e5cf01604a8822
nuwan andra <nuwanandra@live.com>
2023-03-20 17:08:30 +0530

    Testing...

--- App.js
+++ App.js
@@ -10,55 +10,35 @@ import {
   Linking,
   ScrollView,
   RefreshControl,
+  FlatList,
 } from 'react-native';
 
 const App = () => {
   const [items, setItems] = useState([
-    {key: 1, value: 'item 1'},
-    {key: 2, value: 'item 2'},
-    {key: 3, value: 'item 3'},
-    {key: 4, value: 'item 4'},
-    {key: 5, value: 'item 5'},
-    {key: 6, value: 'item 6'},
-    {key: 7, value: 'item 7'},
-    {key: 8, value: 'item 8'},
-    {key: 9, value: 'item 9'},
-    {key: 10, value: 'item 10'},
+    {key: 1, value: 'val1'},
+    {key: 2, value: 'val2'},
+    {key: 3, value: 'val3'},
+    {key: 4, value: 'val4'},
+    {key: 5, value: 'val5'},
   ]);
 
-  const [refreshing, setRefreshing] = useState(false);
-
-  const refreshF = () => {
-    setRefreshing(true);
-    setItems([...items, {key: 11, value: 'item 11'}]);
-    setRefreshing(false);
-  };
-
   return (
-    <ScrollView
-      horizontal={false}
-      style={styles.body}
-      refreshControl={
-        <RefreshControl
-          refreshing={refreshing}
-          onRefresh={refreshF}
-          colors={['blue']}></RefreshControl>
-      }>
-      <StatusBar hidden={true}></StatusBar>
-      {items.map(n => {
-        return (
-          <View style={styles.item} key={n.key}>
-            <Text style={styles.text}>{n.value}</Text>
-          </View>
-        );
-      })}
-    </ScrollView>
+    <FlatList
+      data={items}
+      renderItem={({item}) => (
+        <View onPress={() => this._onPress(item)}>
+          <Text>{item.title}</Text>
+        <View/>
+      )}/>
+
+        
+      
   );
 };
 
 const styles = StyleSheet.create({
   body: {
-    flex: 1,
+    //flex: 1,
     //backgroundColor: 'blue',
     //justifyContent: 'center',
     //alignItems: 'center',
